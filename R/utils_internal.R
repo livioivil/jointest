ec34d03cdaaabd3ad88c66222ebee60a882079c5
Angela Andreella <36375295+angeella@users.noreply.github.com>
2026-06-08 16:25:48 +0200

    bug flip2sss case with 1 obs

--- R/utils_internal.R
+++ R/utils_internal.R
@@ -171,6 +171,12 @@ makedata2lev <- function(data, cluster, summstats_within,model.matrix.between) {
   result_df <- do.call(rbind, dfs_filled)
   rownames(result_df) <- NULL 
   
+  single <- data.frame(table(cluster))$cluster[data.frame(table(cluster))$Freq < 2]
+
+  if(!is.null(single)){
+    result_df <- result_df[!apply(result_df, 1, function(x) any(is.na(x))),]
+  }
+  
   return(result_df) 
 } 
 
